4217ab456ddd2a75476e05b28f805b6c6d3f4f02
NiKiToSchika <147913765+NiKiToSchika@users.noreply.github.com>
2025-04-20 18:54:24 +0300

    Изменен класс BinaryCalculator

--- src/BinaryCalculator.java
+++ src/BinaryCalculator.java
@@ -1,17 +1,22 @@
+// Класс BinaryCalculator расширяет абстрактный класс Calculator
+// и реализует его методы для работы с двоичной системой счисления.
 public class BinaryCalculator extends Calculator {
 
+    // Метод parse переопределяет родительский метод и преобразует строку из двоичной системы в десятичное число
     @Override
     public int parse(String input) {
-        return Integer.parseInt(input, 2);
+        return Integer.parseInt(input, 2); // преобразование из двоичной строки в int
     }
 
+    // Метод format переопределяет родительский метод и преобразует десятичное число обратно в строку в двоичной системе
     @Override
     public String format(int number) {
-        return Integer.toBinaryString(number);
+        return Integer.toBinaryString(number); // преобразование int в строку в двоичной системе
     }
 
+    // Метод возвращает название используемой системы счисления (для идентификации и отображения)
     @Override
     public String getSystemName() {
-        return "BIN";
+        return "BIN"; // обозначение двоичной системы
     }
 }
